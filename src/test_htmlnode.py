4ea30855955597ef1ec14701be7450e58d57365a
Negzaxis <chris.w.preston@gmail.com>
2025-04-28 00:36:22 -0400

    E: added LeafNode & tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 # Tests for  HTMLNode
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
 
@@ -20,4 +20,29 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode(props={})
         self.assertEqual(node.props_to_html(), '')
 
-        
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a_with_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.example.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.example.com">Click me!</a>')
+
+    def test_leaf_to_html_multiple_props(self):
+        node = LeafNode("input", "Text", {"type": "text", "name": "username", "placeholder": "Enter username"})
+        self.assertEqual(node.to_html(), '<input type="text" name="username" placeholder="Enter username">Text</input>')
+
+    def test_leaf_to_html_no_props(self):
+        node = LeafNode("span", "Some text")
+        self.assertEqual(node.to_html(), "<span>Some text</span>")
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Just some text")
+        self.assertEqual(node.to_html(), "Just some text")
+
+    def test_leaf_to_html_no_value(self):
+        with self.assertRaises(ValueError):
+            LeafNode("p", None).to_html()
+
+if __name__ == "__main__":
+    unittest.main()
